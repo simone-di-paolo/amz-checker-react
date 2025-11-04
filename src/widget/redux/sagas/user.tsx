f0ef0933cd6eee9fa51a19efd42eac491265de8a
s.dipaolo90 <s.dipaolo90@gmail.com>
2025-11-04 18:17:49 +0000

    Test update saga

--- src/widget/redux/sagas/user.tsx
+++ src/widget/redux/sagas/user.tsx
@@ -4,12 +4,15 @@ import { FETCH_USER } from '../actions/user';
 
 // Worker saga will be fired on USER_FETCH_REQUESTED actions
 function* incrementAsync() {
+  console.log('Saga Iniziata');
   yield delay(5000);
   yield put({ type: FETCH_USER._SUCCESS });
+  console.log('Saga Finita');
 }
 
 // Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
 // Allows concurrent fetches of user.
 export function* watchIncrementAsync() {
+  console.log('Saga triggerato');
   yield takeLatest(FETCH_USER._REQUEST, incrementAsync);
 }
